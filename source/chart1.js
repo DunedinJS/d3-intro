9dc5bb822478dc398b77c3e41a026b15772875b3
Matthew Norrish <matt.norrish@clavisinsight.com>
2017-03-15 09:14:17 +1300

    add Chart 1 -- a basic bar chart in HTML

---
+++ source/chart1.js
@@ -0,0 +1,24 @@
+import * as d3 from 'd3';
+
+const max = 100;
+
+export default function chart1(id) {
+  // generate an array of eight random numbers between 0 and the max value
+  const data = Array(8).fill(null).map(() => Math.floor(Math.random() * max));
+
+  // select the DOM element which D3 will render into
+  d3.select(id)
+    // select all child divs -- but there are none so an empty selection is made
+    .selectAll('div')
+    // bind the data array to the selection
+    .data(data)
+    // 'enter' the selection
+    // this returns placeholder nodes for each datum (item in the data array) which does not have a corresponding DOM element
+    // because the selection is empty it gives placeholders for all eight items
+    .enter()
+    // now we perform DOM manipulations based on each datum
+    // create the DOM element for each datum
+    .append('div')
+    // set inline style height for each div element based on the corresponding datum
+    .style('height', d => `${d}px`);
+}
--- source/index.js
+++ source/index.js
@@ -1,5 +1,8 @@
 import * as d3 from 'd3';
+import chart1 from './chart1';
 import 'normalize.css';
 import './styles.css';
 
 console.log(`D3 ${d3.version}`);
+
+chart1('#chart1');
